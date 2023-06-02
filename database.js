e763723696ee38d3ffacad44fc6be17a8b32bc53
Ozayo03 <2428260@students.wits.ac.za>
2023-06-02 08:49:15 +0200

    connecting to server database

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 //here we are connectong to the database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'cmt',
-    user: 'root',
-    password: ''
+    host: '10.100.15.124',
+    database: 'cmtdb2',
+    user: 'admin',
+    password: 'password'
 });
 
 connection.connect(function(error){
