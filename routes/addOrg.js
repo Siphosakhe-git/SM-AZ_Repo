cac291a9e8b44ec89c2715b891ee92528ff6e316
zwavhudimunarini <131265276+zwavhudimunarini@users.noreply.github.com>
2023-06-02 09:12:24 +0200

    Update addOrg.js

--- routes/addOrg.js
+++ routes/addOrg.js
@@ -8,7 +8,7 @@ router.post('/addOrg',function(req,res){
   var ainfo = (req.body.addOrg).split(",");
   query =`
   INSERT INTO organiser (CONF_ID,USER_ID, USER_NAME, USER_EMAIL) VALUES ("${req.session.confid}","${ainfo[0]}","${ainfo[2]}","${ainfo[1]}")
-  `;
+  `;//inserting the added person by the admin as an organiser
   database.query(query,function(error){
     if(error){
       res.send('Organiser Already Added!');
@@ -16,7 +16,7 @@ router.post('/addOrg',function(req,res){
     else{
       query1 = `
       SELECT * FROM organiser WHERE CONF_ID ="${req.session.confid}"
-      `;
+      `;//get relevant data
       database.query(query1,(error,data)=>{
         if(error){
           res.send(error);
@@ -25,7 +25,7 @@ router.post('/addOrg',function(req,res){
           req.session.orgs = data;
           query2 = `
           SELECT * FROM areachair WHERE CONF_ID ="${req.session.confid}"
-          `;
+          `;//get relevant data
           database.query(query2,(error,dataa)=>{
             if(error){
               res.send(error);
@@ -34,7 +34,7 @@ router.post('/addOrg',function(req,res){
               req.session.ac = dataa;
               query3 = `
               SELECT * FROM reviewer WHERE CONF_ID ="${req.session.confid}"
-              `;
+              `;//get relevant data
               database.query(query3,(error,datar)=>{
                 if(error){
                   res.send(error);
